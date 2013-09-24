b7c52b103816a5494d948bbc824a496a95d66e2b
mvanger <mvanger@nyu.edu>
2013-09-24 17:21:20 -0500

    reorganization

--- chapter_one/HelloPrinter.java
+++
@@ -1,7 +0,0 @@
-public class HelloPrinter
-{
-  public static void main(String[] args)
-  {
-    System.out.println("Hello, World! It's my first Java program.");
-  }
-}
---
+++ chapter_one/hello/HelloPrinter.java
@@ -0,0 +1,10 @@
+public class HelloPrinter // Declares the class HelloPrinter
+{
+  // main is a method
+  // function or procedure is strictly a method in Java
+  public static void main(String[] args)
+  {
+    // System.out.println is a method within the main method
+    System.out.println("Hello, World! It's my first Java program.");
+  }
+}
