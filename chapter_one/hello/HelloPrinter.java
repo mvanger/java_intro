32e6fec276a68202a45fe89e67f1b965e201b132
mvanger <mvanger@nyu.edu>
2013-09-24 17:57:20 -0500

    first lecture

--- chapter_one/hello/HelloPrinter.java
+++ chapter_one/hello/HelloPrinter.java
@@ -5,6 +5,19 @@ public class HelloPrinter // Declares the class HelloPrinter
   public static void main(String[] args)
   {
     // System.out.println is a method within the main method
-    System.out.println("Hello, World! It's my first Java program.");
+    // Strings have to be enclosed in double quotations
+    // Use a \ to escape a character
+    // System.out.print() will not add a new line after
+    // System.out.println() will
+    // Java is strongly typed
+    System.out.println("Hello, World! It's my second Java program.");
+    System.out.println("Here is an extra \" quotation mark");
+    System.out.println("Here is a \n new line");
+    System.out.println("Here is a \nnew line without the extra space");
   }
 }
+
+// Declaring a variable requires the type and the name
+// Defining a variable sets the content
+// for example "int cansPerPack;" is a declaration
+// and "int cansPerPack = 6;" is a definition
