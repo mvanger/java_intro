3a92c276fb470041aca5624273e891879d903c27
mvanger <mvanger@nyu.edu>
2013-10-20 18:13:04 -0500

    Program works

--- SquareRoot/SquareRoot.java
+++ SquareRoot/SquareRoot.java
@@ -3,21 +3,28 @@ import java.util.Scanner;
 public class SquareRoot {
   public static void main(String[] args)
   {
-    Scanner in = new Scanner(System.in);
-    System.out.println("What number would you like to take the square root of?");
-    int inputNumber = in.nextInt();
-    System.out.println("What would you like the error bound to be?");
-    double inputError = in.nextDouble();
-    double guess = (double) inputNumber / 2;
-    double test = computeBabylonianRoot(inputNumber, guess, inputError);
-    int test2 = computeStartingNumber(inputNumber);
-
-    System.out.println(test2);
+    promptUser("y");
   }
 
-  public static double computeBabylonianRoot(int square, double estimate, double error) {
-    double result = (estimate + (square / estimate)) / 2.0;
-    return result;
+  public static double[] computeBabylonianRoot(int square, double[] estimateIterations, double error) {
+    double iterations = estimateIterations[1] + 1;
+    double[] newEstimate = {(estimateIterations[0] + (square / estimateIterations[0])) / 2.0, iterations};
+    double newError = computeError(square, newEstimate[0]);
+    if (newError > error && newError > 0) {
+      double[] answer = computeBabylonianRoot(square, newEstimate, error);
+      return answer;
+    } else if (newError < 0 && newError * -1 > error) {
+      double[] answer = computeBabylonianRoot(square, newEstimate, error);
+      return answer;
+    } else {
+      return newEstimate;
+    }
+  }
+
+  public static double computeError(int square, double estimate) {
+    double actualRoot = Math.sqrt(square);
+    double newError = (estimate - actualRoot) / actualRoot;
+    return newError;
   }
 
   public static int computeStartingNumber(int square) {
@@ -36,5 +43,38 @@ public class SquareRoot {
       }
     }
     return startingEstimate;
+  }
+
+  public static String promptUser(String input) {
+    if (input.equals("y")) {
+      Scanner in = new Scanner(System.in);
+      System.out.println("What number would you like to take the square root of?");
+      if (!in.hasNextInt()) {
+        System.out.println("Error, starting over.");
+        promptUser("y");
+      } else {
+        int beginSquare = in.nextInt();
+        System.out.println("Within what relative error?");
+        if (!in.hasNextDouble()) {
+          System.out.println("Error, starting over.");
+          promptUser("y");
+        } else {
+          double beginError = in.nextDouble();
+          int startingEstimate = computeStartingNumber(beginSquare);
+          double[] arr = {startingEstimate, 0};
+          double[] test = computeBabylonianRoot(beginSquare, arr, beginError);
+          System.out.println("The answer is " + test[0]);
+          System.out.println("It took " + test[1] + " iterations.");
+          System.out.println("Compute another? (y/n)");
+          String newUserInput = in.next();
+          promptUser(newUserInput);
+        }
+      }
+    } else if (input.equals("n")) {
+      System.out.println("Goodbye!");
+    } else {
+      System.out.println("Error, shutting down.");
+    }
+    return "";
   }
 }
