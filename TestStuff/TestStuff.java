880ebf4ef742039957a23ee587cdf4e1702703be
mvanger <mvanger@nyu.edu>
2013-11-04 21:58:38 -0600

    midterm prep

---
+++ TestStuff/TestStuff.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class TestStuff
+{
+  public static void main(String[] args)
+  {
+    final double SOMETHING = 2;
+    // Reassigning a final variable gives a compile-time error
+    // SOMETHING = 3;
+    String test = "Here is a string";
+    double[] test2 = {1, 2, 3, 4, 5};
+    ArrayList<String> test3 = new ArrayList<String>();
+    test3.add("Mike");
+    test3.add("Brian");
+    // System.out.println(test3);
+    String s = "Howdy";
+    int k = s.length();
+
+    // for (int i = 1; i < k; i++) {
+    //   int j = 0;
+    //   while (j <= i) {
+    //     j++;
+    //     System.out.print(s.substring(i, i+2));
+    //   }
+    //   System.out.println();
+    // }
+    System.out.println(pyramidVolume(5, 3));
+  }
+
+  public static double pyramidVolume(double height, double baseLength)
+  {
+    double baseArea = baseLength * baseLength;
+    return height * baseArea / 3;
+  }
+}
