7feb767cefa9ab777702fcc4894cab8ae3074f6c
mvanger <mvanger@nyu.edu>
2013-11-19 23:11:14 -0600

    done with project 3

--- FlightPlanner/FlightPlanner.java
+++ FlightPlanner/FlightPlanner.java
@@ -1,9 +1,11 @@
-/*
+/**
 MSIA 490 JAVA PROJECT 3
 Group:
     Nicole Lim
     Michael Vanger
     Nina (Kathryn?) Wolf
+Purpose: This program interfaces with two files to keep track of flight bookings.
+General Design: Four options exist for asking the user for input. They can make a new booking, see all bookings, see the bookings for a specific passenger, or the bookings for a specific flight. There also exist methods to read and write from and to the relevant files. The prompt user method wraps these other methods.
 */
 
 import java.io.File;
@@ -19,8 +21,10 @@ public class FlightPlanner
   }
 
   /**
-   * Presents the user with a list of opitions that they can conduct with this program
-   */
+    Presents the user with a list of opitions that they can conduct with this program
+    @params none
+    @return none
+  */
   public static void promptUser() throws FileNotFoundException
   {
     System.out.println("Welcome to the Flight Planner");
@@ -30,8 +34,8 @@ public class FlightPlanner
     System.out.println("U - In order to show all the reservations for a particular user");
     System.out.println("B - In order to show the names of the customers who have bookings for a particular flight");
     System.out.println("Q - In order to quit this application");
-    Scanner test = new Scanner(System.in);
-    String input = test.next();
+    Scanner firstIn = new Scanner(System.in);
+    String input = firstIn.next();
 
     // Calls proper method
     if (input.toLowerCase().equals("n")) {
@@ -53,28 +57,27 @@ public class FlightPlanner
       System.out.println("Invalid input. Please enter a valid option:");
       promptUser();
     }
-
-    // test.close();
   }
 
   /**
-   * Reads in the input file of flight information
-   * @return An array with origin city, destination city and flight#
-   */
+    Reads in the input file of flight information
+    @params none
+    @return An array with origin city, destination city, and flight#
+  */
   public static String[][] readFlightFile() throws FileNotFoundException
   {
-        //Reads flight information text file
+    //Reads flight information text file
     File inputFile = new File("allFlights.txt");
     Scanner secondIn = new Scanner(inputFile);
 
-    //New array created
+    //New flight array created
     String[][] flightArray = new String[100][3];
 
     int i = 0;
 
     while (secondIn.hasNextLine())
     {
-        //Scans each line for origin, destination, and flight #
+      //Scans each line for origin, destination, and flight #
       String line = secondIn.nextLine();
       Scanner lineScanner = new Scanner(line);
       lineScanner.useDelimiter(";");
@@ -93,20 +96,17 @@ public class FlightPlanner
       i++;
       lineScanner.close();
     }
-
-    // secondIn.close();
-
     return flightArray;
   }
 
   /**
-   * inputs the file that contains flight information
-   * @return an array of with passenger name, origin city, destination city and flight#
-   */
+    Reads the file that contains passenger/flight information
+    @params none
+    @return an array of with passenger name, origin city, destination city, and flight#
+  */
   public static String[][] readUserFile() throws FileNotFoundException
   {
-        //Reads existing user reservation text file and converts to array
-
+    //Reads existing user reservation text file and converts to array
     File inputFile = new File("reservations.txt");
     Scanner thirdIn = new Scanner(inputFile);
     String[][] userArray = new String[100][4];
@@ -134,16 +134,14 @@ public class FlightPlanner
       i++;
       lineScanner.close();
     }
-
-    // thirdIn.close();
-    // inputFile.closeFile();
     return userArray;
   }
 
-/**
- * Writes new reservations to a text file and saves
- * @param outputArray an array with passenger name, origin city, destination city and flight#
- **/
+  /**
+    Writes new reservations to a text file and saves
+    @param outputArray an array with passenger name, origin city, destination city, and flight#
+    @return none
+    */
   public static void writeUserFile(String[][] outputArray) throws FileNotFoundException
   {
     int i = 0;
@@ -162,9 +160,10 @@ public class FlightPlanner
   }
 
   /**
-   * Creates new reservations
-   * @param flightArray an array with origin city, destination city and flight#, userArray an array with passenger name, origin city, destination city and flight#
-   */
+    Creates new reservations
+    @params flightArray an array with origin city, destination city and flight#, userArray an array with passenger name, origin city, destination city and flight#
+    @return none
+  */
   public static void nMethod(String[][] flightArray, String[][] userArray) throws FileNotFoundException
   {
     // Declares 4 arrays for reservation information
@@ -196,7 +195,6 @@ public class FlightPlanner
     String userDeparture = fourthIn.nextLine();
     System.out.println("Enter the destination city: ");
     String userDestination = fourthIn.nextLine();
-    // fourthIn.close();
 
     nameArray[i] = userName;
     departureArray[i] = userDeparture;
@@ -217,7 +215,6 @@ public class FlightPlanner
     if (check == 0)
     {
       System.out.println("No matching flights found.");
-      // System.exit(0);
     }
 
     //Writes the new reservations to the reservations text file & saves it
@@ -230,20 +227,36 @@ public class FlightPlanner
     newOutputArray[k][3] = flightNoArray[k];
     k++;
     }
-    writeUserFile(newOutputArray);
 
+    // Checks existing combinations
+    boolean userExists = false;
+    for (int x = 0; x < i; x++) {
+      if (newOutputArray[i][0].equals(newOutputArray[x][0]) && newOutputArray[i][3].equals(newOutputArray[x][3])) {
+        userExists = true;
+        break;
+      }
+    }
+    if (!userExists) {
+      writeUserFile(newOutputArray);
+    } else {
+      System.out.println("Passenger is already booked on that flight.");
+    }
+
+    System.out.println();
     promptUser();
   }
 
   /**
-   * shows all the data for the existing reservations for all the users
-   */
+    Shows all the data for the existing reservations for all the users
+    @params none
+    @return none
+  */
   public static void sMethod() throws FileNotFoundException
   {
-    // Prints out existing reservations
     // Reads users file
     String[][] outputArray = readUserFile();
 
+    System.out.println("All flights and passengers:");
     //Loops through array and prints out every reservation in the array
     for (int i = 0; i < 100; i++) {
       for (int j = 0; j < 4; j++) {
@@ -259,15 +272,19 @@ public class FlightPlanner
     if (outputArray[0][0] == null) {
       System.out.println("No reservations were found.");
     }
+
+    System.out.println();
     promptUser();
   }
 
-   /**
-   * shows all the reservations for a particular user
-   */
+  /**
+    Shows all the reservations for a particular user
+    @params none
+    @return none
+  */
   public static void uMethod() throws FileNotFoundException
   {
-        // Gets passenger name from user
+    // Gets passenger name from user
     Scanner fifthIn = new Scanner(System.in);
     System.out.println("Enter the name of a passenger you would like to find reservations for: ");
     String passenger = fifthIn.nextLine();
@@ -276,6 +293,7 @@ public class FlightPlanner
     String[][] outputArray = readUserFile();
     // Declares an integer to check if user exists
     int check = 0;
+    System.out.println("Flights:");
     // Loops through array to check for user
     for (int i = 0; i < 100; i++) {
       // If passenger is found, prints relevant info
@@ -295,23 +313,36 @@ public class FlightPlanner
       System.out.println("Passenger not found.");
     }
 
-    // fifthIn.close();
+    System.out.println();
     promptUser();
   }
 
   /**
-   * show the names of the customers who have bookings for a particular flight
-   */
+    Show the names of the customers who have bookings for a particular flight
+    @params none
+    @return none
+  */
   public static void bMethod() throws FileNotFoundException
   {
-        // Gets flight number from user
+    // Gets flight number from user
     Scanner sixthIn = new Scanner(System.in);
     System.out.println("Enter a flight number:");
-    String flightNumber = sixthIn.nextLine();
+    // Instantiates empty string
+    String flightNumber = "";
+    // Checks for valid flight number input
+    while (!flightNumber.matches("\\d{5}") || flightNumber.length() != 5) {
+      if (sixthIn.hasNextLine()) {
+        flightNumber = sixthIn.nextLine();
+      }
+      if (!flightNumber.matches("\\d{5}")) {
+        System.out.println("Invalid flight number. Flight # is 5 digits.");
+      }
+    }
     // Reads users file
     String[][] outputArray = readUserFile();
     // Declares an integer to check if flight exists
     int check = 0;
+    System.out.println("Passengers:");
     // Loops through array to check for flight
     for (int i = 0; i < 100; i++) {
       // If flight is found, prints passenger info
@@ -331,7 +362,7 @@ public class FlightPlanner
       System.out.println("Flight not found, or there are no passengers.");
     }
 
-    // sixthIn.close();
+    System.out.println();
     promptUser();
   }
 }
